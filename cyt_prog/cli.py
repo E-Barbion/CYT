646fbfc5d5f9e2bc570b1f09b99bab65a2d2ea6b
Erik.Barbion <erik.barbion@stud-tu.darmstadt.de>
2025-12-14 07:16:00 +0100

    fixed I/O

--- cyt_prog/cli.py
+++ cyt_prog/cli.py
@@ -16,12 +16,30 @@ def main():
             OUTPUT_PATH = "cyt_out/default/"
             print("Note : No output directory given. Output will be at " + OUTPUT_PATH)
     else: sys.exit(print("Error: no file given! exiting..."))
+    print("debug1 : results of I/O parsing...")
     print("SOURCE_NAME: " + SOURCE_NAME)
     print("SOURCE_PATH: " + SOURCE_PATH)
     print("OUTPUT_NAME: " + OUTPUT_NAME)
     print("OUTPUT_PATH: " + OUTPUT_PATH)
 
-    out_dir = Path(OUTPUT_PATH) # OUTPUT_PATH = "cty_out/default/"
+    # Invariant:
+    # - cyt_out/<OUTPUT_NAME>/ is deleted and recreated at program start
+    # - After initialization, the directory exists and is empty
+    # - No existence checks are required for files inside it
+    out_dir = Path(OUTPUT_PATH)
+    ast = out_dir / "ast.txt"
+    ri = out_dir / "ir.txt"
+    c = out_dir / f"{OUTPUT_NAME}.c"
+    build = out_dir / "build.log"
+    runtime = out_dir / "runtime/"
     if out_dir.exists():
         shutil.rmtree(out_dir)
     out_dir.mkdir(parents=True)
+    ast.touch()
+    ri.touch()
+    c.touch()
+    build.touch()
+    runtime.mkdir()
+    print("debug1 : created files / directories...")
+    for entry in out_dir.iterdir():
+        print(entry)
