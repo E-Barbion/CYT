b7c85fa93ced8cfc71f4cc7b7cb8bf3c05fa9ddc
Erik.Barbion <erik.barbion@stud-tu.darmstadt.de>
2025-12-14 06:41:36 +0100

    Fixed I/O parsing

--- cyt_prog/cli.py
+++ cyt_prog/cli.py
@@ -4,21 +4,22 @@ from pathlib import Path
 
 def main():
     if len(sys.argv) > 1:
-        for s in sys.argv[1].split("/"):
-            if s.endswith(".py"):
-                SOURCE_NAME = s.strip(".py")
+        dirs1 = sys.argv[1].split("/")
+        SOURCE_NAME = dirs1[len(dirs1)-1].strip(".py")
         SOURCE_PATH = sys.argv[1]
         if len(sys.argv) > 2:
-            OUTPUT_NAME = "cyt_out/" + sys.argv[2].strip() + "/"
+            dirs2 = sys.argv[2].split("/")
+            OUTPUT_NAME = dirs2[len(dirs2)-1]
+            OUTPUT_PATH = "cyt_out/" + OUTPUT_NAME
         else:
             OUTPUT_NAME = "default"
             OUTPUT_PATH = "cyt_out/default/"
             print("Note : No output directory given. Output will be at " + OUTPUT_PATH)
     else: sys.exit(print("Error: no file given! exiting..."))
-    # print("SOURCE_NAME: " + SOURCE_NAME)
-    # print("SOURCE_PATH: " + SOURCE_PATH)
-    # print("OUTPUT_NAME: " + OUTPUT_NAME)
-    # print("OUTPUT_PATH: " + OUTPUT_PATH)
+    print("SOURCE_NAME: " + SOURCE_NAME)
+    print("SOURCE_PATH: " + SOURCE_PATH)
+    print("OUTPUT_NAME: " + OUTPUT_NAME)
+    print("OUTPUT_PATH: " + OUTPUT_PATH)
 
     out_dir = Path(OUTPUT_PATH) # OUTPUT_PATH = "cty_out/default/"
     if out_dir.exists():
--- info.md
+++ info.md
@@ -1,2 +1,7 @@
 # ./cyt
 This is the executable that launches CYT. Do not touch.
+
+# ./.gitignore
+This file contains directories ignored by git.
+printf "/path/to/thing\n" >> .gitignore             # Adds file / directory to .gitignore
+sed -n '1,200p' .gitignore                          # Lists contents of .gitignore
